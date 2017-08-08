501c9be11dce6b043778a4ad36a24eba955a49d9
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-08-07 20:35:17 -0600

    Add front end for sample chat app.

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -55,8 +55,24 @@ socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
 let channel = socket.channel("room:lobby", {})
+let chatInput  = document.querySelector("#chat-input");
+let messagesContainer = document.querySelector("#messages");
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
+
+chatInput.addEventListener("keypress", event => {
+  if( event.keyCode === 13){
+    channel.push("new_msg", { body: chatInput.value })
+    chatInput.value = ""
+  }
+});
+
+channel.on("new_msg", payload => {
+  console.log (payload);
+  let messageItem = document.createElement("li");
+  messageItem.innerText = `[${Date()}] ${payload.body}`
+  messagesContainer.appendChild(messageItem)
+})
 
 export default socket
