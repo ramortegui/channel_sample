1e8e141f907195dc5692a07d5d219ca663e1cd2c
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-08-07 19:15:51 -0600

    Create and enable room channel.

--- lib/channel_sample_web/channels/user_socket.ex
+++ lib/channel_sample_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ChannelSampleWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ChannelSampleWeb.RoomChannel
+  channel "room:*", ChannelSampleWeb.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
